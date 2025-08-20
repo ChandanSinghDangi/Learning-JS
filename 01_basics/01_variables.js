7dbdfbd6dc3beddf41552940115f5095c7b40d61
style <chandandangi429@gmail.com>
2025-08-20 16:08:22 +0000

    fifth commit

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -2,12 +2,12 @@
 const accountId = 12345;
 let username = 'chandan';
 var userCity = 'haldwani'
-userGmail = 'user@gmail.com'
+userGmail = 'user@gmail.com' // not a good at all to initlize the variable
 
 
 // accountId = 1; // not allowed with const
-username = 'hello'; // let value can be changed...
-userCity = 'Bhimtal';
+// username = 'hello'; // let value can be changed...
+// userCity = 'Bhimtal';
 userGmail = 'userNew@gmail.com';
 
 
@@ -16,9 +16,30 @@ userGmail = 'userNew@gmail.com';
 
 
 // this is shotcut to print all the variables in one console...
-console.table([username, userCity, userGmail]);
+// console.table([username, userCity, userGmail]);
 
 
+/* 
 
+Prefer not to use var. because of issue in block scope and functional scope
+
+*/
+
+if(true) {
+    userCity = 'New York'
+    username = 'chandan@gmail.com'
+    var newName = 'chandan';
+    let age = 10;
+    // console.log(userCity);
+    // console.log(username);
+    
+    
+}
+
+// newName = 'kanchan';
+// console.log(newName);
+
+// age = 12;
+console.log(age);
 
 
