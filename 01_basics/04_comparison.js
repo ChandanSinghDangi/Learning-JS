133272d9671acd32ba9201b7446fa58d52048fa6
style <chandandangi429@gmail.com>
2025-08-21 16:44:51 +0000

    Comparison in JS

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,59 @@
+
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 <= 1);
+// console.log(2 != 1);
+// console.log(2 == 1);
+// console.log(2 === 1);
+
+
+// console.log('2' > 1); // true
+// console.log('02' > 1); // true
+
+/* 
+
+ Always remember when comparing two values.
+ They should have similar dataTypes. because it may give unexpacted results...
+ So avoid comparing them......
+
+*/
+
+
+// this are weird comparsion that don't give expacted results...
+
+// console.log(null > 0);
+// console.log(null < 0);
+// console.log(null === 0);
+// console.log(null >= 0);
+// console.log(null != 0);
+
+
+// console.log(undefined == 0);
+// console.log(undefined === 0);
+// console.log(undefined > 0);
+// console.log(undefined < 0);
+// console.log(undefined >= 0);
+// console.log(undefined != 0);
+
+/* 
+
+ In JS comparsion >,< and so On, vs equality check ==, === they both work differently or give different results... 
+ and even among == and === they both worked different...
+
+*/
+
+
+// console.log('2' == 2);
+// console.log('2' === 2); // this is strict check ===
+
+
+
+
+
+
+
+
+
+
+
