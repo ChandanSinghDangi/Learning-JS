0eecef56bb6431a84c2e885c5a7ed5e905ebf8ba
style <chandandangi429@gmail.com>
2025-08-21 15:50:39 +0000

    seventh commit for conversion of dataTypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,100 @@
+
+
+// console.log(typeof NaN); // it's type of will be number...
+
+
+// let score = 33;
+// console.log(typeof score);
+
+
+// let scores = '33';
+// console.log(typeof scores);
+// let valueInNumber = Number(scores);
+// console.log(typeof valueInNumber);
+
+
+// let newScores = '22abc';
+// console.log(typeof newScores);
+// let valueInNumber2 = Number(newScores);
+// // this give number as typeof which is not right. cause our value is 22abc which is not a number for sure!
+// console.log(typeof valueInNumber2); // typeof is a number
+// console.log(valueInNumber2); // NaN is a value 
+
+
+// let newScores = null;
+// console.log(typeof newScores);
+// let valueInNumber2 = Number(newScores);
+// console.log(typeof valueInNumber2); // type of is number 
+// console.log(valueInNumber2); // but value stored is 0
+
+
+// let newScores = undefined;
+// console.log(typeof newScores); // type of is undefined
+// let valueInNumber2 = Number(newScores);
+// console.log(typeof valueInNumber2); // type of is number 
+// console.log(valueInNumber2); // but value stored is NaN
+
+
+// let newScores = true;
+// console.log(typeof newScores); // type of is boolean
+// let valueInNumber2 = Number(newScores);
+// console.log(typeof valueInNumber2); // type of is number 
+// console.log(valueInNumber2); // but value stored is 1
+
+
+
+// let isLoggedIn = 1;
+// let isBooleanLoggedIn = Boolean(isLoggedIn);
+// console.log(typeof isBooleanLoggedIn); // type of Boolean
+// console.log(isBooleanLoggedIn); // value is true;
+
+
+// let isLoggedIn = 0;
+// let isBooleanLoggedIn = Boolean(isLoggedIn);
+// console.log(typeof isBooleanLoggedIn); // type of Boolean
+// console.log(isBooleanLoggedIn); // value is false;
+
+
+// let isLoggedIn = '';
+// let isBooleanLoggedIn = Boolean(isLoggedIn);
+// console.log(typeof isBooleanLoggedIn); // type of Boolean
+// console.log(isBooleanLoggedIn); // value is false;
+
+
+// let isLoggedIn = 'chandan';
+// let isBooleanLoggedIn = Boolean(isLoggedIn);
+// console.log(typeof isBooleanLoggedIn); // type of Boolean
+// console.log(isBooleanLoggedIn); // value is true;
+
+
+// let randomNumber = 10;
+// let numberToSring = String(randomNumber);
+// console.log(typeof numberToSring); // typeof is string
+// console.log(numberToSring); // value is 10
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
