0d738fc76fcca9629998e3834fea96a63c83e6c5
style <chandandangi429@gmail.com>
2025-08-22 16:04:02 +0000

    add dataType summery

---
+++ 01_basics/dataType-summery.js
@@ -0,0 +1,14 @@
+
+// Premitive Data Types:-
+
+// 7 types:- String, number, Boolearn, null, undefined, Symbol, BigInt
+
+
+// Non Premitive Data Types:- Also known as (reference )
+
+// Array, Object, Functions
+
+
+
+
+
