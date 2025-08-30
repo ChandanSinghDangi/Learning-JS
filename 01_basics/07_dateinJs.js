d807bbea47a869604fd613f14d8584e79638dff2
style <chandandangi429@gmail.com>
2025-08-30 15:45:25 +0000

    learning Date and Time

---
+++ 01_basics/07_dateinJs.js
@@ -0,0 +1,27 @@
+
+// let a = 10;
+// console.log(a);
+
+
+// console.log(Temporal.Now.instant()); // not rolled out yet...
+
+
+// let myDate =  Date();
+// console.log(myDate);
+// console.log("this is myDate :: ",typeof myDate);
+
+
+// let myDate_2 = new Date();
+// console.log('this is myDate_2 :: ',myDate_2);
+// console.log('this is myDate_2 typeof :: ',typeof myDate_2);
+// console.log('this is toDateString :: ',myDate_2.toDateString());
+// console.log('this is toISOString ::',myDate_2.toISOString());
+// console.log('this is toJSON :: ',myDate_2.toJSON());
+// console.log('this is toLocalDateString :: ',myDate_2.toLocaleDateString());
+// console.log('this is toLocalString :: ',myDate_2.toLocaleString());
+
+
+// console.log(Date.now()); // this is will run...
+
+
+
